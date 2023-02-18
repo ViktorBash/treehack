3937bd676bffcdedb173e46bc5d99e088008bd68
ViktorBash <basharkevichv@gmail.com>
2023-02-18 10:10:25 -0800

    Got working oauth login into google

--- popup.js
+++ popup.js
@@ -1,1 +1,18 @@
 console.log("This is a popup!")
+
+// Step 1: Authenticate with gmail API (Can be hardcoded in exclusively for one user)
+// Step 2: Get the last 24 hours of emails in a digestible form
+
+
+window.onload = function() {
+  document.querySelector('button').addEventListener('click', function() {
+    try {
+    chrome.identity.getAuthToken({interactive: true}, function(token) {
+      console.log(token);
+    });
+  }
+  catch (e) {
+    console.log(e);
+  }
+  });
+};
