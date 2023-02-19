e199920de1cdf10a5041d2b8422a4a4beeccbdb4
ViktorBash <basharkevichv@gmail.com>
2023-02-18 22:52:59 -0800

    Finished parsing emails into strings

--- popup.js
+++ popup.js
@@ -1,18 +1,45 @@
-console.log("This is a popup!")
-
 // Step 1: Authenticate with gmail API (Can be hardcoded in exclusively for one user)
 // Step 2: Get the last 24 hours of emails in a digestible form
 
+// var moduleName = 'base64url';
+// require([moduleName], function(Module) {
+//   // 
+//   console.log("hi");
+// });
+// const base64url = require('base64url');
+var base64Reference = require('base64url');
+global.window.base64url = base64Reference;
+
+let emailArray = [];
+
+function getGmailData(token) {
+  fetch('https://www.googleapis.com/oauth2/v1/userinfo?alt=json', {json: true, headers: {'Authorization': 'Bearer ' + token}})
+  .then((userInfoResponse) => userInfoResponse.json()).then((userInfoResponse) => {
+    // console.log(userInfoResponse);
+    fetch(`https://content-gmail.googleapis.com/gmail/v1/users/${userInfoResponse.id}/messages?maxResults=10`, {json: true, headers: {'Authorization': 'Bearer ' + token}})
+    .then((messageListResponse) => messageListResponse.json()).then((messageListResponse) => {
+      // Now get the data for each email
+      // console.log(messageListResponse)
+      for (let index = 0; index < 10; index++) {
+        let emailId = messageListResponse.messages[index].id;
+        fetch(`https://gmail.googleapis.com/gmail/v1/users/${userInfoResponse.id}/messages/${emailId}`, {json: true, headers: {'Authorization': 'Bearer ' + token}})
+        .then((individualEmailResponse) => individualEmailResponse.json()).then((individualEmailResponse) => {
+          // console.log(individualEmailResponse);
+          const base64Encoded = individualEmailResponse.payload.parts[0].body.data;
+          let advancedDecoded = base64url.decode(base64Encoded);
+          emailArray.push(advancedDecoded);
+        });
+      }
+      console.log(emailArray);
+    });
+  })
+}
 
 window.onload = function() {
   document.querySelector('button').addEventListener('click', function() {
-    try {
     chrome.identity.getAuthToken({interactive: true}, function(token) {
-      console.log(token);
+      console.log("User Token: " + token);
+      getGmailData(token);
     });
-  }
-  catch (e) {
-    console.log(e);
-  }
   });
 };
